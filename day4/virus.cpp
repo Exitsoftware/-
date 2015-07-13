28358ae99451e60c5672a3ed4b5fd20cfdbb6aaf
NaYunHwan <kbk9288@gmail.com>
2015-07-13 09:48:47 +0900

    day4
    day4

--- day4/virus.cpp
+++ day4/virus.cpp
@@ -1,1 +1,46 @@
 #include "stdio.h"
+
+int arr[1000][1000]={0};
+int visit[1000] = {0};
+int virus = 0;
+int computer_count;
+
+void func(int n){
+	visit[n] = 1;	
+	for(int i = 1; i <= computer_count; i++){
+		if(arr[n][i] == 1 && visit[i] == 0){
+			virus++;
+			func(i);
+		}
+	}
+}
+
+
+int main(){
+	
+
+	
+	int num;
+
+	scanf("%d", &computer_count);
+	scanf("%d", &num);
+
+	while(num--){
+		int a,b;
+		scanf("%d %d", &a, &b);
+		arr[a][b] = 1;
+		arr[b][a] = 1;
+
+	}
+
+	func(1);
+	// for(int i = 1; i <= computer_count; i++){
+	// 	for(int j = 1; j <= computer_count; j++){
+	// 		printf("%d ", arr[i][j]);
+	// 	}
+	// 	printf("\n");
+	// }
+	// printf("\n");
+	printf("%d\n", virus);
+	return 0;
+}
